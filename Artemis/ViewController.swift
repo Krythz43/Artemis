d0a818c125b84dc21c4bba948559fa50114d9653
Krithick Santhosh N <krithickrockz@gmail.com>
2023-01-11 15:39:34 +0530

    Initial commit

---
+++ Artemis/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Artemis
+//
+//  Created by Krithick Santhosh on 11/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
